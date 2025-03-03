bed8479b4a43eacab3bea346837e795f496b55a6
Gabriele Spada <gabcloud@pm.me>
2025-03-03 14:33:05 +0100

    Remove useless return

--- src/utils/ideal_bosons.rs
+++ src/utils/ideal_bosons.rs
@@ -240,8 +240,8 @@ pub fn compute_ln_partition_functions(
 pub fn ideal_gas_energy(particles: usize, t_over_tc0: f64) -> f64 {
     let (z1, dz1) = single_particle_z_dz(particles, t_over_tc0);
     let (_lnz, dlnz) = compute_ln_partition_functions(z1, dz1);
-    let energy = -dlnz[particles] / particles as f64;
-    return energy;
+    // Compute energy
+    -dlnz[particles] / particles as f64
 }
 
 #[cfg(test)]
